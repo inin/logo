28c5d4f97ee348ea28ad6d1dfc289697bfd6de0a
John Korb <korbjt@gmail.com>
2014-03-16 22:38:07 -0400

    add logstash appender

--- appender_builtin.go
+++ appender_builtin.go
@@ -1,13 +1,23 @@
 package logo
 
 import (
+	"encoding/json"
+	"io"
 	"log"
 	"os"
 )
 
 func NewStdoutAppender() Appender {
 	return &stdoutAppender{
-		log.New(os.Stdout, "", 0), //use logger for its buffering
+		log.New(os.Stdout, "", 0), //use std logger for its buffering
+	}
+}
+
+func NewLogstashAppender(w io.Writer, v LogstashVersion, pretty bool) Appender {
+	return &logstashAppender{
+		log.New(w, "", 0),
+		v,
+		pretty,
 	}
 }
 
@@ -22,4 +32,53 @@ func (s *stdoutAppender) Write(message *LogMessage) {
 }
 
 func (s *stdoutAppender) Close() { /* noop */
+}
+
+type LogstashVersion uint8
+
+const (
+	//logstash version 0
+	LSV0 LogstashVersion = iota
+
+	//logstash version 1
+	LSV1
+)
+
+type logstashAppender struct {
+	logger  *log.Logger
+	version LogstashVersion
+	pretty  bool
+}
+
+const logstashLayout = "2006-01-02T15:04:05.000000Z"
+
+func (l *logstashAppender) Write(message *LogMessage) {
+	var out interface{}
+	switch l.version {
+	case LSV0:
+		msg := make(map[string]interface{})
+		message.MDC["level"] = message.Level.String()
+		msg["@fields"] = message.MDC
+		msg["@message"] = message.Message
+		msg["@timestamp"] = message.Timestamp.Format(logstashLayout)
+		out = msg
+	default: //use version 1
+		msg := message.MDC
+		msg["level"] = message.Level.String()
+		msg["@message"] = message.Message
+		msg["@timestamp"] = message.Timestamp.Format(logstashLayout)
+		out = msg
+	}
+
+	var logData []byte
+	if l.pretty {
+		logData, _ = json.MarshalIndent(out, "", "  ")
+	} else {
+		logData, _ = json.Marshal(out)
+	}
+
+	l.logger.Print(string(logData))
+}
+
+func (l *logstashAppender) Close() { /* noop */
 }
