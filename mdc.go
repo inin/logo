f1b13a270e02b0f65325178d465b2e2df2a4e440
John Korb <korbjt@gmail.com>
2014-03-16 21:42:51 -0400

    use reader writer lock instead of standard lock

--- mdc.go
+++ mdc.go
@@ -6,33 +6,54 @@ import (
 )
 
 type MDC struct {
-	mu   sync.Mutex
+	mu   sync.RWMutex
 	data map[string]string
 }
 
-func NewMDC(data map[string]string) *MDC {
+func NewMDC() *MDC {
 	return &MDC{
-		sync.Mutex{},
-		data,
+		sync.RWMutex{},
+		make(map[string]string),
+	}
+}
+
+func MDCFromMDC(ctx *MDC) *MDC {
+	return &MDC{
+		sync.RWMutex{},
+		ctx.snapshot(),
+	}
+}
+
+func MDCFromMap(ctx map[string]string) *MDC {
+	return &MDC{
+		sync.RWMutex{},
+		ctx,
 	}
 }
 
 func (m *MDC) Get(key string) (string, bool) {
-	m.mu.Lock()
-	defer m.mu.Unlock()
+	//aquire a read lock
+	m.mu.RLock()
+	defer m.mu.RUnlock()
+
 	val, ok := m.data[key]
 	return val, ok
 }
 
+//Put adds the string value of 'value' to the context
 func (m *MDC) Put(key string, value interface{}) {
+	//aquire a write lock
 	m.mu.Lock()
 	defer m.mu.Unlock()
+
 	m.data[key] = fmt.Sprintf("%v", value)
 }
 
 func (m *MDC) snapshot() map[string]string {
-	m.mu.Lock()
-	defer m.mu.Unlock()
+	//aquire a read lock
+	m.mu.RLock()
+	defer m.mu.RUnlock()
+
 	data := make(map[string]string, len(m.data))
 	for key, value := range m.data {
 		data[key] = value
